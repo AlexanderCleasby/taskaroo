a4e16fa3fdb8b7320fc8c90ebd56d172ef562c78
alexandercleasby <cleasby.alex@gmail.com>
2019-04-28 11:39:40 -0400

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_24_001519) do
+ActiveRecord::Schema.define(version: 2019_04_27_192322) do
+
+  create_table "lists", force: :cascade do |t|
+    t.integer "owner_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer "list_id"
+    t.boolean "completed"
+    t.string "title"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+  end
+
+  create_table "users_lists", force: :cascade do |t|
+    t.integer "list_id"
+    t.integer "user_id"
+    t.string "privilege"
   end
 
 end
