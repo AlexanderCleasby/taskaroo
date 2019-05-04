45d5c363c76c03565653870c598e367be8867ec4
alexandercleasby <cleasby.alex@gmail.com>
2019-05-04 18:08:18 -0400

    require login for certain controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
+require 'pry'
 class ApplicationController < ActionController::Base
+
+    private
+
+    def require_logged_in
+        redirect_to controller: 'sessions', action: 'new' unless session[:user_id]
+    end
+
 end
