76627cd879e144011428f8abaef1da97638932a0
alexandercleasby <cleasby.alex@gmail.com>
2019-05-11 13:39:47 -0400

    added canwrite model method for list and used it  to conditioanlly render the add task form.

--- app/models/list.rb
+++ app/models/list.rb
@@ -7,4 +7,15 @@ class List < ApplicationRecord
     def owner
         User.find(owner_id)
     end
+
+    def canwrite(id)
+        if self.owner_id==id
+            return true
+        elseif self.user_lists.find_by({user_id:id})
+            return self.user_lists.find_by({user_id:id}).privilege
+        else
+            false
+        end
+    end
+
 end
